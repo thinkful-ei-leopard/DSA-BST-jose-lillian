b90bbe70547db03c576a04fc166ce75c368bf0e5
Lillian Burnside <lillian.burnside@gmail.com>
2020-04-07 13:16:12 -0500

    drill 5

--- bst-drills.js
+++ bst-drills.js
@@ -1,17 +1,18 @@
-const BinarySearchTree = require('./bst')
+'use strict';
+const BinarySearchTree = require('./bst');
 
-// drill 3
-// let BST = new BinarySearchTree()
-// BST.insert(3)
-// BST.insert(1)
-// BST.insert(4)
-// BST.insert(6)
-// BST.insert(9)
-// BST.insert(2)
-// BST.insert(5)
-// BST.insert(7)
+//drill 3
+let BST = new BinarySearchTree()
+BST.insert(3)
+BST.insert(1)
+BST.insert(4)
+BST.insert(6)
+BST.insert(9)
+BST.insert(2)
+BST.insert(5)
+BST.insert(7)
 
-// console.log(BST)
+//console.log(BST)
 
 // function tree(t){
 //     if(!t){
@@ -42,3 +43,19 @@ const BinarySearchTree = require('./bst')
 // drill 4
 // complexity exponential O(2^n)
 // returns the sum of the entire tree
+
+// drill 5
+
+function height(tree, count = 0) {
+  if(tree === null){
+    return 0;
+  }
+
+  if(tree.left === null && tree.right === null){
+    return count;
+  }
+  
+  return Math.max(height(tree.right, count+1), height(tree.left, count + 1));
+}
+
+console.log(height(BST));
